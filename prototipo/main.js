337bd5fa8dc8346e09fb5b84f66c826472393c9e
Alicia Alderete <aliciavanesa9@gmail.com>
2023-10-16 15:39:51 -0300

    nuevos cambios

--- prototipo/main.js
+++ prototipo/main.js
@@ -1,46 +1,46 @@
-
 /* SALUDO */
 let nombre = "Alicia";
 let apellido = "Alderete";
 let saludo = "Hola";
 console.log(`${saludo} ${nombre} ${apellido}, Bienvenida!`);
 
-/*USUARIO*/
-let usuario = ("Ingrese el usuario");
-while(usuario != "Ingrese el usuario"){
-    switch (usuario) {
-        case "Alicia":
-            alert("Hola Alicia");
-            break;
-    
-        default:
-            alert("Usuario incorrecto")
-            break;
-    }
-usuario = ("Ingrese el usuario")
-}
-console.log("Siguiente paso")
+/*USUARIO*/ 
+let usuario, contrasena;
+do {
+    usuario = prompt("Ingrese un nombre de usuario:");
+    contrasena = prompt("Ingrese una contraseña:");
+} while (!usuario || !contrasena);
+
+alert("Usuario creado: " + usuario);
+alert("Contraseña creada: " + contrasena);
 
 /* CONTRASEÑA */
-let contrasena = "alicia720";
-if (contrasena === "alicia720") {
-    console.log ("Contraseña correcta");
-} else{ 
+let contrasenia = "alicia720";
+if (contrasenia === "alicia720") {
+    console.log("Contraseña correcta");
+} else {
     console.log("Usuario o contraseña incorrecto")
 }
 
 /*EDAD*/
 let edad = 18;
-if(edad >= 18) {
+if (edad >= 18) {
     console.log("Puedes ingresar a nuestro sitio");
-} else{
+} else {
     console.log("No estas autorizado a realizar compras");
 }
 
-/*NUMERO PARA SORTEO*/
-for (let numero = 1; numero <= 20; turno++) {
-    let nombre = prompt("Ingrese su nombre para participar por una orden de compra")
-
-    alert(`Numero: #${numero} Nombre: ${nombre}`);
+/*TURNO PARA INGRESAR A NUESTRO SHOWROOM*/
+for(let turno = 1; turno <= 10; turno++){
+    let nombre = prompt("Ingrese su nombre para nuestros sorteos semanales");
+    alert(`Turno #${turno} Nombre: ${nombre}`);
 }
-alert("Numeros agotados,sorteamos por nuesto Instagram el 10/10/23")
+alert("Turnos agotados, proxima semana abrimos cupos nuevamente")
+
+/*SORTEO POR UNA ORDEN DE COMPRA*/
+function sorteoAleatorio(minimo, maximo){
+    return Math.floor(Math.random() * (maximo - minimo +1) + minimo);
+}
+for(let i = 1; i <= 30; ++i){
+    console.log(sorteoAleatorio(1, 20));
+}
